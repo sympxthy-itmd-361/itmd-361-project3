33972d023425ec1c77927b3a2342e930416ed7bb
Jony Zhang <yzhang121@hawk.iit.edu>
2018-04-19 16:37:44 +0800

    Add a dynamic caption on rotating picture display;

--- js/rotatingPicture.js
+++ js/rotatingPicture.js
@@ -2,7 +2,7 @@
 
 var slides = document.getElementById("slides");
 var slideList = slides.children;
-var nSlides = slideList.length;
+var nSlides = slideList.length - 1;
 var i = -1;
 
 for (i = 0; i < nSlides; ++i) {
@@ -16,6 +16,8 @@ if (i == -1) {
   i = 0;
 }
 
+var slideCaption = document.getElementById("slide-caption");
+
 setInterval(function() {
   console.log("rotating...");
   slide = slideList[i];
@@ -23,4 +25,6 @@ setInterval(function() {
   i = (i + 1) % nSlides;
   slide = slideList[i];
   slide.classList.remove("hidden");
+  var img = slide.children[0];
+  slideCaption.innerText = img.alt;
 }, 2000);
